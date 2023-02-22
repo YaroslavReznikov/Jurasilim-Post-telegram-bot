1a88bdf467db89413035c04268b739a0041f95be
yaroslav <reznikovaroslav419@gmail.com>
2023-02-22 17:27:08 +0200

    the libraty was changed in order to add buttons from telebot to aiogram

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ token = "5744717544:AAE0s0J_X8zz1EW3zexj1dW4tYdsoTVCCxY"
 bot = Bot(token)
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands=['help', 'start'])
-async def send_welcome_message(mesage:types.Message):
+async def send_welcome_message(mesage: types.Message):
     await mesage.reply("Hi")
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
