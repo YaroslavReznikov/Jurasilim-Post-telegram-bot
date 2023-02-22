37ecb7d8b19716c45c979b6224dc389a7a4d1500
yaroslav <reznikovaroslav419@gmail.com>
2023-02-22 15:58:57 +0200

    some insignifacant changes

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import telebot
 import requests
 
-
 token = "5744717544:AAE0s0J_X8zz1EW3zexj1dW4tYdsoTVCCxY"
 bot = telebot.TeleBot(token)
+
+
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome_message(mesage):
     bot.reply_to(mesage, 'I am working')
