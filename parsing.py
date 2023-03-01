dbf09ad570f69188747c8bf5a2c2402f2800b465
yaroslav <reznikovaroslav419@gmail.com>
2023-03-01 18:43:09 +0200

    Was added connection to the database

--- parsing.py
+++ parsing.py
@@ -1,7 +1,9 @@
 import requests
 import time
 import xml.etree.ElementTree as ET
-from datetime import  datetime
+from datetime import datetime
+from setup import password, database_name, host, user
+import pymysql
 
 
 def g(func):
@@ -15,10 +17,18 @@ def g(func):
 
 
 
-
 class parsing_part:
     def __init__(self):
         self.urls_list = []
+        self.database = pymysql.connect(
+            host=host,
+            password=password,
+            database=database_name,
+            user=user,
+            port=3306,
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
     @g
     def get_links(self, url):
         root = ET.fromstring(requests.get(url).text)
@@ -28,4 +38,7 @@ class parsing_part:
 
     def database_sending(self):
         print(self.urls_list)
-#0.6857387000000001
+
+    def send_links_to_user(self):
+        print('does_something_with_database')
+# 0.6857387000000001
---
+++ setup.py
@@ -0,0 +1,5 @@
+host = "local_host"
+user = "mysql root"
+password = "8l*lE$a3xFdoiiR*hGj8D^4rOks8"
+database_name = "jerusalem post"
+
