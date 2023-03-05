f9aa6e6971cef7be192729003c7aa9e5e9b42546
yaroslav <reznikovaroslav419@gmail.com>
2023-03-05 16:58:48 +0200

    Final built

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ token = "5744717544:AAE0s0J_X8zz1EW3zexj1dW4tYdsoTVCCxY"
 bot = telebot.TeleBot(token)
 
 pars = parsing.parsing_part()
-links = pars.send_links_to_user()
+
 schedule.every(120).seconds.do(pars.get_links)
-pars.feel_the_rss_database_database()
 pars.get_links()
 while True:
     schedule.run_pending()
     @bot.message_handler(commands=['new'])
     def send_new(message):
+        links = pars.send_links_to_user()
         for _ in range(5):
             id, url, date, topic = next(links)
             bot.send_message(message.chat.id, text=F"{date.strftime('%d: %m: %Y, %H:%M')}'\n' {topic} '\n' {url}")
