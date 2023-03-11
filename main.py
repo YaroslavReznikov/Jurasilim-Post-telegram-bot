6b3ee4a8585094f427955590a9763ee5bfe6694a
yaroslav <reznikovaroslav419@gmail.com>
2023-03-11 08:32:06 +0200

    change for auto launch

--- main.py
+++ main.py
@@ -4,7 +4,6 @@
 import parsing
 import telebot
 from time import time
-import asyncio
 
 
 token = "5744717544:AAE0s0J_X8zz1EW3zexj1dW4tYdsoTVCCxY"
@@ -24,13 +23,6 @@ def send_new(message):
         bot.send_message(message.chat.id, text=F"{date.strftime('%d.%m.%Y, %H:%M')}\n{topic.strip()}\n{url.strip()}")
         pars.update_sended(id)
 
-async def update():
-    global last_time
-    while True:
-        if last_time + 10 >= time():
-            last_time = time()
-            pars.get_links()
-        await asyncio.sleep(1)
 
 
 @bot.message_handler(commands=['check'])
@@ -39,6 +31,3 @@ def admin_check(message):
 
     id, url, date, topic = next(pars.send_links_to_user())
     bot.send_message(message.chat.id, text=F"{date.strftime('%d.%m.%Y, %H:%M')}\n{topic.strip()}\n{url.strip()}")
-asyncio.create_task(update())
-asyncio.get_event_loop().run_until_complete(bot.polling())
-
