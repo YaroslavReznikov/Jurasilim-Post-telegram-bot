760c0ab85fe7d10619c6364c336e184c2f31f0d7
yaroslav <reznikovaroslav419@gmail.com>
2023-03-14 16:20:58 +0200

    final fix

--- main.py
+++ main.py
@@ -5,29 +5,31 @@ import parsing
 import telebot
 from time import time
 
+try:
+    token = "5744717544:AAE0s0J_X8zz1EW3zexj1dW4tYdsoTVCCxY"
+    bot = telebot.TeleBot(token)
+except:
+    pass
+else:
+    pars = parsing.parsing_part()
 
-token = "5744717544:AAE0s0J_X8zz1EW3zexj1dW4tYdsoTVCCxY"
-bot = telebot.TeleBot(token)
-
-pars = parsing.parsing_part()
-
-last_time = time()
-#pars.get_links()
+    last_time = time()
+    #pars.get_links()
 
 
-@bot.message_handler(commands=['new'])
-def send_new(message):
-    links = pars.send_links_to_user()
-    for _ in range(5):
-        id, url, date, topic = next(links)
+    @bot.message_handler(commands=['new'])
+    def send_new(message):
+        links = pars.send_links_to_user()
+        for _ in range(5):
+            id, url, date, topic = next(links)
+            bot.send_message(message.chat.id, text=F"{date.strftime('%d.%m.%Y, %H:%M')}\n{topic.strip()}\n{url.strip()}")
+            pars.update_sended(id)
+
+
+
+    @bot.message_handler(commands=['check'])
+    def admin_check(message):
+        id, url, date, topic = next(pars.send_links_to_user())
         bot.send_message(message.chat.id, text=F"{date.strftime('%d.%m.%Y, %H:%M')}\n{topic.strip()}\n{url.strip()}")
-        pars.update_sended(id)
 
-
-
-@bot.message_handler(commands=['check'])
-def admin_check(message):
-    id, url, date, topic = next(pars.send_links_to_user())
-    bot.send_message(message.chat.id, text=F"{date.strftime('%d.%m.%Y, %H:%M')}\n{topic.strip()}\n{url.strip()}")
-
-bot.polling(none_stop=True)
+    bot.polling(none_stop=True)
