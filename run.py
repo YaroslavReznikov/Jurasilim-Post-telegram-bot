490b188882d38d4be216a8ada766097674cf3b07
yaroslav <reznikovaroslav419@gmail.com>
2023-04-01 10:03:25 +0300

    final fix

--- run.py
+++ run.py
@@ -2,12 +2,5 @@ from main import main
 
 
 
-does_work = False
-
-if not does_work:
-    does_work = True
-    try:
-        main()
-    except Exception as exsp:
-        print(Exception)
-        does_work = False
+while True:
+    main()
