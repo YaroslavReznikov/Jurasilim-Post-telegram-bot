df7f8becdb0e63c71ed408423e8de3502f1cd5bd
yaroslav <reznikovaroslav419@gmail.com>
2023-05-23 21:48:40 +0300

    We added an option to add new news source as well as we made some optimization

--- main.py
+++ main.py
@@ -37,8 +37,7 @@ def main():
             pars.add_url(message.chat.id, news_id)
             bot.send_message(message.chat.id,
                              text=F"{date.strftime('%d.%m.%Y, %H:%M')}\n{topic.strip()}\n{url.strip()}")
-        del links
-
+        bot.send_message(message.chat.id, text = 'ghgh')
     def admin_check(message):
         links = pars.find_links_for_user(message.chat.id, 1)
         for url, date, news_id, channels_row, topic in links:
